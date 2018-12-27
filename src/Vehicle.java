2041c3ba66c73f4fa31f6227a17f2e150510ff8d
tox_94 <toni.bakarcic@fer.hr>
2018-12-27 23:25:07 +0100

    Class for storing vehicles created.

---
+++ src/Vehicle.java
@@ -0,0 +1,50 @@
+public class Vehicle {
+    private int vehicleLength;
+    private int series;
+    private int departure;
+    private int layoutType;
+
+    public Vehicle(int vehicleLength, int series, int departure, int layoutType) {
+        this.vehicleLength = vehicleLength;
+        this.series = series;
+        this.departure = departure;
+        this.layoutType = layoutType;
+    }
+
+    public int getVehicleLength() {
+        return vehicleLength;
+    }
+
+    public void setVehicleLength(int vehicleLength) {
+        this.vehicleLength = vehicleLength;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(int departure) {
+        this.departure = departure;
+    }
+
+    public int getLayoutType() {
+        return layoutType;
+    }
+
+    public void setLayoutType(int layoutType) {
+        this.layoutType = layoutType;
+    }
+
+    @Override
+    public String toString() {
+        return "Len = " + vehicleLength + ", ser = " + series + ", dep = " + departure + ", lay = " + layoutType;
+    }
+}
