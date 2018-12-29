af304814afa2e546045944fac116e6c19faf1682
tox_94 <toni.bakarcic@fer.hr>
2018-12-29 18:30:38 +0100

    Method for getting all vehicles in a track created.

--- src/Track.java
+++ src/Track.java
@@ -47,10 +47,23 @@ public class Track {
         }
     }
 
+    public String printVehiclesInTrack() {
+        if (parkedVehicles.isEmpty()) {
+            return "\n";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < parkedVehicles.size() - 1; i++) {
+                sb.append(parkedVehicles.get(i).getOrd() + " ");
+            }
+            sb.append(parkedVehicles.get(parkedVehicles.size() - 1));
+            return sb.toString();
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Len = " + trackLength + ", restrictions : ");
+        sb.append("Num = " + ord + "len = " + trackLength + ", restrictions : ");
         for (int i = 0; i < restrictions.size() - 1; i++) {
             sb.append(restrictions.get(i) + ", ");
         }
